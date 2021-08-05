5f3d32f97c802ab0fda76e0975a958d592515625
Adnan Niaz <sanixstudio@gmail.com>
2021-08-04 23:29:01 -0700

    restore point

--- api/index.js
+++ api/index.js
@@ -1,37 +1,27 @@
-// required Node modules
+// required Node MODULES
 const express = require('express')
 const morgan = require('morgan')
 const chalk = require('chalk')
 const mongoose = require('mongoose')
-require('dotenv').config();
 
 const app = express()
 
-// middleware
+require('dotenv').config();
+require('./config/database')
+
+
+// MIDDLEWARE
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(morgan('common'))
 
-// routes
-app.use('/', (req, res) => {})
+// ROUTES
+const userRoutes = require('./routes/users')
+const authRoutes = require('./routes/auth')
+// app.use('/api', userRoutes)
+app.use('/api', authRoutes)
 
-// Database connection
-async function dbConnect() {
-  try {
-    const db = await mongoose.connect(process.env.DB_URI, {
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useNewUrlParser: true,
-    })
-    db && console.log(chalk.yellow('Connected to database'))
-  } catch (err) { console.log(err) }
-}
-
-dbConnect()
-
-app.get('/', (req, res) => res.send("Welcome"))
-
-// server
+// SERVER
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
   console.log(chalk.yellow('listening at http://localhost:' + PORT))
